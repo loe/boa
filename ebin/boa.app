d3f986cfc7ce6a78a1f0e055c46208124ebfa7d0
W. Andrew Loe III <andrew@andrewloe.com>
2012-11-08 17:30:30 -0800

    Generated skeleton boa application.

---
+++ ebin/boa.app
@@ -0,0 +1,8 @@
+{application,boa,
+             [{description,"API Constrictor"},
+              {vsn,"1"},
+              {registered,[]},
+              {modules,[boa_app,boa_sup]},
+              {applications,[kernel,stdlib]},
+              {mod,{boa_app,[]}},
+              {env,[]}]}.
---
+++ src/boa_app.erl
@@ -0,0 +1,16 @@
+-module(boa_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    boa_sup:start_link().
+
+stop(_State) ->
+    ok.
