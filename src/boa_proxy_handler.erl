a212cee12c6b71cb7bb65e9df25e0f9d7540b187
W. Andrew Loe III <andrew@andrewloe.com>
2012-11-09 15:57:21 -0800

    Dynamically generating most of the components from the intial request.

--- src/boa_proxy_handler.erl
+++ src/boa_proxy_handler.erl
@@ -1,6 +1,3 @@
-%% Feel free to use, reuse and abuse the code in this file.
-
-%% @doc GET echo handler.
 -module(boa_proxy_handler).
 
 -export([init/3]).
@@ -8,15 +5,23 @@
 -export([terminate/2]).
 
 init(_Transport, Req, []) ->
-  {ok, Client} = cowboy_client:init([]),
-  {ok, Req, Client}.
+  {ok, State} = cowboy_client:init([]),
+  {ok, Req, State}.
 
 handle(Req, State) ->
-  {ok, Client2} = cowboy_client:request(<<"GET">>, <<"http://en.wikipedia.org/wiki/Main_Page">>, [], <<>>, State),
-  {ok, Status, Headers, Client3} = cowboy_client:response(Client2),
-  {ok, Body, _} = cowboy_client:response_body(Client3),
-  {ok, Req2} = cowboy_req:reply(Status, Headers, Body, Req),
-  {ok, Req2, State}.
+  %% Break apart the request.
+  {ReqMethod, Req2} = cowboy_req:method(Req),
+  {ReqHeaders, Req3} = cowboy_req:headers(Req2),
+  {ok, ReqBody, Req4} = cowboy_req:body(Req3),
+
+  %% Issue proxy request.
+  {ok, Client2} = cowboy_client:request(ReqMethod, <<"http://en.wikipedia.org/wiki/Main_Page">>, ReqHeaders, ReqBody, State),
+  {ok, ResStatus, ResHeaders, Client3} = cowboy_client:response(Client2),
+  {ok, ResBody, _Client4} = cowboy_client:response_body(Client3),
+
+  %% Reply to initial request.
+  {ok, Res} = cowboy_req:reply(ResStatus, ResHeaders, ResBody, Req4),
+  {ok, Res, State}.
 
 terminate(_Req, _State) ->
   ok.
