10565388336ed7387feb7b13225e9e35f2e8e613
W. Andrew Loe III <andrew@andrewloe.com>
2012-11-12 16:04:10 -0800

    Clean up variable names to be more useful.

--- src/boa_proxy_handler.erl
+++ src/boa_proxy_handler.erl
@@ -5,24 +5,24 @@
 -export([terminate/2]).
 
 init(_Transport, Req, []) ->
-  {ok, State} = cowboy_client:init([]),
-  {ok, Req, State}.
+  {ok, Client} = cowboy_client:init([]),
+  {ok, Req, Client}.
 
-handle(Req, State) ->
+handle(Req, Client) ->
   %% Break apart the request.
   {ReqMethod, Req2} = cowboy_req:method(Req),
   {ReqHeaders, Req3} = cowboy_req:headers(Req2),
-  {ReqURL, Req4} = cowboy_req:url(Req3),
+  {ReqURL, Req4} = cowboy_req:path(Req3),
   {ok, ReqBody, Req5} = cowboy_req:body(Req4),
 
   %% Issue proxy request.
-  {ok, Client2} = cowboy_client:request(ReqMethod, ReqURL, ReqHeaders, ReqBody, State),
+  {ok, Client2} = cowboy_client:request(ReqMethod, ReqURL, ReqHeaders, ReqBody, Client),
   {ok, ResStatus, ResHeaders, Client3} = cowboy_client:response(Client2),
-  {ok, ResBody, _Client4} = cowboy_client:response_body(Client3),
+  {ok, ResBody, Client4} = cowboy_client:response_body(Client3),
 
   %% Reply to initial request.
   {ok, Res} = cowboy_req:reply(ResStatus, ResHeaders, ResBody, Req5),
-  {ok, Res, State}.
+  {ok, Res, Client4}.
 
 terminate(_Req, _State) ->
   ok.
