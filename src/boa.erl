becb48b8a74ddd90b52ffda1114e9220ae2aaab2
W. Andrew Loe III <andrew@andrewloe.com>
2012-11-22 23:21:48 -0700

    Fire up Lager.

--- src/boa.erl
+++ src/boa.erl
@@ -6,7 +6,8 @@
 %% API.
 
 start() ->
-	ok = application:start(crypto),
-	ok = application:start(ranch),
-	ok = application:start(cowboy),
-	ok = application:start(boa).
+  ok = application:start(crypto),
+  ok = application:start(ranch),
+  ok = application:start(lager),
+  ok = application:start(cowboy),
+  ok = application:start(boa).
